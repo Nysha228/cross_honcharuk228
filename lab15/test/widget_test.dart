62e7557f86ee12c55ca666372acc27c86ceb8532
Nysha228 <sashagoncharuk2our@gmail.com>
2024-11-28 02:20:24 +0200

    15 ready

--- lab15/test/widget_test.dart
+++ lab15/test/widget_test.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lab_15/main.dart';
 
+class FunctionCalculator {
+  double calculate(double x) {
+    if (x == 4) {
+      throw ArgumentError('Division by zero is not allowed when x equals 4');
+    }
+    return x * x / (4 - x);
+  }
+}
+
 void main() {
   group('FunctionCalculator tests', () {
-    test('Correctly calculates the function y = (x^2 + x) - 100', () {
+    test('Correctly calculates the function y = x^2 / (4 - x)', () {
       final calculator = FunctionCalculator();
-      expect(calculator.calculate(0), -100);
-      expect(calculator.calculate(10), 10);
-      expect(calculator.calculate(-10), -10);
+      expect(calculator.calculate(0), 0);
+      expect(calculator.calculate(2), 1);
+      expect(calculator.calculate(-2), 4);
+      expect(calculator.calculate(1), 1.0);
+      expect(() => calculator.calculate(4), throwsArgumentError);
     });
   });
 
